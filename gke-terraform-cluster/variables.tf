45f64a79fa9d5dbd331a975dba28e777e3a2281d
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2024-03-28 08:47:02 +0000

    add variables in varibles.tf

---
+++ gke-terraform-cluster/variables.tf
@@ -0,0 +1,13 @@
+variable "gke_username" {
+  default = ""
+  description = "gke username"
+}
+variable "gke_password" {
+  default = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default = 2
+
+}
